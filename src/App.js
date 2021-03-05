fc3b71bfbdedce4ffb1bf763135d9e0c6b2c34b8
prullansky <owenchboy@gmail.com>
2021-03-05 12:53:41 +0100

    Started working on the functional logig of the TypingBox component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useEffect } from 'react'
+import ToDo from './Components/ToDo.js'
+import TypingBox from './Components/TypingBox.js'
 
 function App() {
+  const [toDo, settoDo] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    settoDo(toDo)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TypingBox handleSubmit={handleSubmit} />
+      <ToDo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/TypingBox.js
+++ src/Components/TypingBox.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
 
 export default function TypingBox() {
-  return <div></div>;
+  const [item, setItem] = useState('')
+
+  console.log(item)
+
+  const handleChange = ({ target }) => setItem(target.value)
+
+  return (
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          To-do:
+          <input value={item} type="text" onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
 }
